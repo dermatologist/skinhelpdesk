98e8ce97db776bdc1fd03d8259d1c9ae5baea9ee
dermatologist <github@gulfdoctor.net>
2020-01-06 14:45:41 -0500

    feat: github package repo trial 1

--- notes/create-modules.md
+++ notes/create-modules.md
@@ -7,3 +7,8 @@
 * mvn archetype:generate -DgroupId=in.co.dermatologist  -DartifactId=add-name-here
 
 ## The test_images folder in each module at the same level as stc and target
+
+
+## Deploy to Github Package Repository
+
+* Refer: https://stackoverflow.com/questions/57711558/deploy-to-github-package-registry-from-github-action
--- segment/src/test/java/in/co/dermatologist/ShdSegmentTest.java
+++ segment/src/test/java/in/co/dermatologist/ShdSegmentTest.java
@@ -25,51 +25,51 @@ public class ShdSegmentTest {
         assertTrue(true);
     }
 
-    @Test
-    public void shouldSegmentAndPrintWithIj() throws IOException {
-        String skinhelpdesk = System.getProperty("user.dir");
-        String inpI = new String(Files.readAllBytes(Paths.get(skinhelpdesk + "/test_images/image.txt")));
-        ShdSegment shdSegment = new ShdSegment(inpI);
-        String outI = shdSegment.getOutputImageStr();
-        BufferedWriter writer = new BufferedWriter(new FileWriter(skinhelpdesk + "/test_images/image_out.txt"));
-        writer.write(outI);
-        File outputfile = new File(skinhelpdesk + "/test_images/image_out.jpg");
-        ImageIO.write(ShdUtils.decodeToImage(outI), "jpg", outputfile);
+    // @Test
+    // public void shouldSegmentAndPrintWithIj() throws IOException {
+    //     String skinhelpdesk = System.getProperty("user.dir");
+    //     String inpI = new String(Files.readAllBytes(Paths.get(skinhelpdesk + "/test_images/image.txt")));
+    //     ShdSegment shdSegment = new ShdSegment(inpI);
+    //     String outI = shdSegment.getOutputImageStr();
+    //     BufferedWriter writer = new BufferedWriter(new FileWriter(skinhelpdesk + "/test_images/image_out.txt"));
+    //     writer.write(outI);
+    //     File outputfile = new File(skinhelpdesk + "/test_images/image_out.jpg");
+    //     ImageIO.write(ShdUtils.decodeToImage(outI), "jpg", outputfile);
 
-        writer.close();
-    }
+    //     writer.close();
+    // }
 
-    @Test
-    public void shouldSegmentAndPrintWithKmeans() throws IOException {
-        String skinhelpdesk = System.getProperty("user.dir");
-        String inpI = new String(Files.readAllBytes(Paths.get(skinhelpdesk + "/test_images/image.txt")));
-        ShdKmeansSegment shdKmeansSegment = new ShdKmeansSegment(inpI);
-        String outI = shdKmeansSegment.getOutputImageStr();
-        BufferedWriter writer = new BufferedWriter(new FileWriter(skinhelpdesk + "/test_images/image_out_kmeans.txt"));
-        writer.write(outI);
-        File outputfile = new File(skinhelpdesk + "/test_images/image_out_kmeans.jpg");
-        ImageIO.write(ShdUtils.decodeToImage(outI), "jpg", outputfile);
+    // @Test
+    // public void shouldSegmentAndPrintWithKmeans() throws IOException {
+    //     String skinhelpdesk = System.getProperty("user.dir");
+    //     String inpI = new String(Files.readAllBytes(Paths.get(skinhelpdesk + "/test_images/image.txt")));
+    //     ShdKmeansSegment shdKmeansSegment = new ShdKmeansSegment(inpI);
+    //     String outI = shdKmeansSegment.getOutputImageStr();
+    //     BufferedWriter writer = new BufferedWriter(new FileWriter(skinhelpdesk + "/test_images/image_out_kmeans.txt"));
+    //     writer.write(outI);
+    //     File outputfile = new File(skinhelpdesk + "/test_images/image_out_kmeans.jpg");
+    //     ImageIO.write(ShdUtils.decodeToImage(outI), "jpg", outputfile);
 
-        writer.close();
-    }
+    //     writer.close();
+    // }
 
-    @Test
-    public void shouldSegmentWithKmeansFromImage() throws IOException {
-        String skinhelpdesk = System.getProperty("user.dir");
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File(skinhelpdesk + "/test_images/my-picture.jpg"));
-        } catch (IOException e) {
-        }
-        String inpI = ShdUtils.encodeToString(img, "jpg");
-        // String inpI = new String(Files.readAllBytes(Paths.get(skinhelpdesk + "/test_images/image.txt")));
-        ShdKmeansSegment shdKmeansSegment = new ShdKmeansSegment(inpI);
-        String outI = shdKmeansSegment.getOutputImageStr();
-        BufferedWriter writer = new BufferedWriter(new FileWriter(skinhelpdesk + "/test_images/mypic_out_kmeans.txt"));
-        writer.write(outI);
-        File outputfile = new File(skinhelpdesk + "/test_images/mypic_out_kmeans.jpg");
-        ImageIO.write(ShdUtils.decodeToImage(outI), "jpg", outputfile);
+    // @Test
+    // public void shouldSegmentWithKmeansFromImage() throws IOException {
+    //     String skinhelpdesk = System.getProperty("user.dir");
+    //     BufferedImage img = null;
+    //     try {
+    //         img = ImageIO.read(new File(skinhelpdesk + "/test_images/my-picture.jpg"));
+    //     } catch (IOException e) {
+    //     }
+    //     String inpI = ShdUtils.encodeToString(img, "jpg");
+    //     // String inpI = new String(Files.readAllBytes(Paths.get(skinhelpdesk + "/test_images/image.txt")));
+    //     ShdKmeansSegment shdKmeansSegment = new ShdKmeansSegment(inpI);
+    //     String outI = shdKmeansSegment.getOutputImageStr();
+    //     BufferedWriter writer = new BufferedWriter(new FileWriter(skinhelpdesk + "/test_images/mypic_out_kmeans.txt"));
+    //     writer.write(outI);
+    //     File outputfile = new File(skinhelpdesk + "/test_images/mypic_out_kmeans.jpg");
+    //     ImageIO.write(ShdUtils.decodeToImage(outI), "jpg", outputfile);
 
-        writer.close();
-    }
+    //     writer.close();
+    // }
 }
